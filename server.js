82d5b3080f4fe4cafad3d3b497238d0a96e64bdf
vaniashows <vania6600@gmail.com>
2018-02-24 18:32:57 +0300

    js form input

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-function setMapMarkersByID(inputID) {
-    let request = new XMLHttpRequest();
+function formSubmit() {
 
-    request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            let data = JSON.parse(request.responseText);
-            let pole = data.poles[inputID - 1];
+    let formInput = document.forms["inputForm"]["name"].value;
+    console.log(formInput);
 
-            console.log(pole);
-            //TODO: get ID from HTML form
-        }
+    function setMapMarkersByID(inputID) {
+        let request = new XMLHttpRequest();
 
-        request.open("GET", "/src/database.json", true);
-        request.send();
-    };
+        request.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let data = JSON.parse(request.responseText);
+                let pole = data.poles[inputID - 1];
 
-    function IDFormValidation() {
-        let x = document.forms["myForm"]["value"].value;
-        console.log(x);
-        if (x === "") {
-            alert("Name must be filled out");
+                console.log(pole);
+                //TODO: get ID from HTML form
+            }
+
+            request.open("GET", "/src/database.json", true);
+            request.send();
+        };
+
+        function IDFormValidation() {
+            let x = document.forms["myForm"]["value"].value;
+            console.log(x);
+            if (x === "") {
+                alert("Name must be filled out");
+            }
         }
     }
 }
