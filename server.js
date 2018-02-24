d8cd667a45a16d98555b142f6417124352ce7288
vaniashows <vania6600@gmail.com>
2018-02-24 16:57:40 +0300

    fixed server.js

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-function getPoleCoordinatesByID(inputID) {
+function setMapMarkersByID(inputID) {
     let request = new XMLHttpRequest();
-
-    let pole;
 
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             let data = JSON.parse(request.responseText);
-            pole = data.poles[inputID - 1];
+            let pole = data.poles[inputID - 1];
 
             console.log(pole);
-            return pole;
+            //TODO: get ID from HTML form
+
+
         }
-
     };
-    request.open("GET", "/src/database.json", true);
 
+    request.open("GET", "/src/database.json", true);
     request.send();
 }
 
-let coordinates = getPoleCoordinatesByID(2);
-console.log(coordinates);
+function IDFormValidation() {
+    let x = document.forms["myForm"]["value"].value;
+    console.log(x);
+    if (x === "") {
+        alert("Name must be filled out");
+    }
+}
