8abdd6f5b7e080bdd1cff84d258a8ed4b69db677
vaniashows <vania6600@gmail.com>
2018-02-24 15:33:44 +0300

    added server query support

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-let query = new XMLHttpRequest();
-query.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        let pole = JSON.parse(this.responseText);
-    }
-};
-query.open("GET", "database.json", true);
-query.send();
+function getPoleCoordinatesByID(inputID) {
+    let request = new XMLHttpRequest();
+
+    let pole;
+
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let data = JSON.parse(request.responseText);
+            pole = data.poles[inputID - 1];
+
+            console.log(pole);
+            return pole;
+        }
+
+    };
+    request.open("GET", "/src/database.json", true);
+
+    request.send();
+}
+
+let coordinates = getPoleCoordinatesByID(2);
+console.log(coordinates);
